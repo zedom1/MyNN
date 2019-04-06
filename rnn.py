907ffce1ab3b2cc5a8b272127ac856ea1e5bff4a
zedom1 <w468750@126.com>
2019-04-06 15:37:43 +0800

    提取activation成传入参数，增加early stop，增加rnn的assertion

--- rnn.py
+++ rnn.py
@@ -1,6 +1,4 @@
 import copy, numpy as np
-from activation import Sigmoid
-
 
 class RNN(object):
 
@@ -15,24 +13,28 @@ class RNN(object):
 		self.w_hidden_output = 2*np.random.random((self.hidden_dim, self.output_dim)) - 1
 		self.w_hidden_hidden = 2*np.random.random((self.hidden_dim, self.hidden_dim)) - 1
 
-		self.activation = Sigmoid()
+		self.activation = arg["activation"]()
 
-	def train(self, X, y, learning_rate = 0.1, verbose = False):
+	def train(self, X, y, learning_rate = 0.1):
+
+		assert len(np.shape(X)) == 3, "Invalid input shape. The input must be 3d (batch, timestep, input_dim)"
+		batch_size, steps, input_dim = np.shape(X)
+		assert self.input_dim == input_dim, "Unmatch input_dim."
 
 		deltas = list()
+		overallError = 0
 		predicts = list()
 		hidden_layer = list()
-		batch_size, steps, _ = np.shape(X)
+		
 		hidden_layer.append(np.zeros([batch_size, self.hidden_dim]))
 		# Forwarding
 		# moving along the positions in the binary encoding
-		overallError = 0
 
 		for position in range(steps):
 
 			# generate input and output
-			current_X = np.atleast_2d(X[:, position])
-			current_y = np.atleast_2d(y[:, position])
+			current_X = np.atleast_2d(X[:batch_size, position])
+			current_y = np.atleast_2d(y[:batch_size, position])
 			# hidden layer (input ~+ pre_hidden)
 			new_hidden = self.activation._calculate((np.dot(current_X, self.w_input_hidden) + np.dot(hidden_layer[-1], self.w_hidden_hidden)))
 			# output layer (new binary representation)
@@ -57,7 +59,7 @@ class RNN(object):
 		# Backward update
 		for position in range(steps):
 
-			current_X =  np.atleast_2d(X[:, steps - position - 1])
+			current_X =  np.atleast_2d(X[:batch_size, steps - position - 1])
 			now_hidden_layer = np.atleast_2d(hidden_layer[-position-1])
 			pre_hidden_layer = np.atleast_2d(hidden_layer[-position-2])
 			current_deltas = deltas[-position-1]
@@ -66,15 +68,12 @@ class RNN(object):
 			w_ho_update += np.expand_dims(np.mean(delta_w_ho, axis=1), axis=1)/batch_size
 
 			delta_c_h = current_deltas.dot(self.w_hidden_output.T)
-			
 			delta_c_h += np.atleast_2d(accumulate_hidden_delta).dot(self.w_hidden_hidden)
-
 			delta_c_h *= self.activation._derivative_with_calculate(now_hidden_layer)
+			
 			accumulate_hidden_delta = delta_c_h
-
-			accumulate_derivative = accumulate_hidden_delta 
-			w_ih_update += current_X.T.dot(accumulate_derivative)/batch_size
-			w_hh_update += pre_hidden_layer.T.dot(accumulate_derivative)/batch_size
+			w_ih_update += current_X.T.dot(delta_c_h)/batch_size
+			w_hh_update += pre_hidden_layer.T.dot(delta_c_h)/batch_size
 			
 
 		# update using gradient descent
